c3d0bfec7b550c1f4ebe67a1121c8eb60bd1e9ec
Taha Ahvazi <tahax.dev@gmail.com>
2023-11-03 23:20:18 +0330

    Java Scope

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
-    static int myMethod(int x){return x;}
-    static int myMethod(int x, int y){return x + y;}
     public static void main(String[] args) {
-//----------🌍(Method Overloading)🌍--------------------------
-        // function -> Method -> function same name -> Different functionality based on the inputs
-        int result1 = myMethod(10);
-        int result2 = myMethod(10,5);
-        System.out.println(result2);
+//----------🌍(Java Scope)🌍--------------------------
+       // We can not use myNumber here
+        int myNumber = 20;
+        // We can use myNumber here
+        System.out.println(myNumber);
+        {
+            int yourNumber = 30;
+            System.out.println(yourNumber);
+        }
+//       System.out.println(yourNumber);
     }
 }
