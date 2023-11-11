10ee09242d2214e2c6fc4c8d8cb56b64682c6327
Taha Ahvazi <tahax.dev@gmail.com>
2023-11-11 14:36:55 +0330

    Java class Attributes

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
-public class Main {
-    public static class Number{
-        int x = 10;
-    }
-    public static void main(String[] args) {
-//--------💪(Java Classes and Objects)💪--------------------------
-        Number myNumberObject = new Number();
-        System.out.println("This is The value of class object: " + myNumberObject.x);
+//-----------💪(Java Class Attributes)💪-----------------
+ public class Main {
+    int x = 150;
+    int y = 200;
+    String myName = "Taha";
+    String myCity = "Ahvaz";
+    public static void main(String[] args){
+        Main myFirstObject =  new Main();
+        Main mySecoundObject = new Main();
+
+        myFirstObject.x = 205;
+        myFirstObject.y = 100;
+
+        mySecoundObject.x  = 1;
+        mySecoundObject.y = 2;
+
+        System.out.println(myFirstObject.x + myFirstObject.y);
+        System.out.println(mySecoundObject.y + mySecoundObject.x);
     }
 }
