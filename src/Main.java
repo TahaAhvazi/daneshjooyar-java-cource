8eb4cf6fe693acd39e15ef661b368df6e76c7f78
Taha Ahvazi <tahax.dev@gmail.com>
2023-11-13 00:06:48 +0330

    Java Encapsulation

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,13 @@
-//-----------💪(Java Constructors - Part2 )💪-----------------
+//---------💪(Java Encapsulation)💪---------------------------
 public class Main {
-    // Constructor -> Special Method
-    // Whenever we create an Object
-    int x;
+    // Private -> Setter , Getter
 
-    // Default Constructor
-    public Main() {
-        x = 12;
-    }
-
-    public Main(int y) {
-        x = y;
-    }
-
-    public Main(int y, int u) {
-        x = y * u;
-    }
-
-    public static void main(String[] args) {
-        Main myObject = new Main(45);
-        Main secoundObject = new Main(10,5);
-        System.out.println(myObject.x);
-        System.out.println(secoundObject.x);
+    public static void main(String[] args){
+        Person personObject = new Person();
+        personObject.setName("Taha");
+        System.out.println(personObject.getName());
     }
 }
+
+
+
