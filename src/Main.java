7cb5897ad872b7e5bcfe144ec4fd1d96c51c99bb
Taha Ahvazi <tahax.dev@gmail.com>
2023-11-12 21:18:34 +0330

    Java Constructors - Part 2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
-//-----------💪(Java Constructors - Part1 )💪-----------------
+//-----------💪(Java Constructors - Part2 )💪-----------------
 public class Main {
     // Constructor -> Special Method
     // Whenever we create an Object
     int x;
-    public Main(){
+
+    // Default Constructor
+    public Main() {
         x = 12;
     }
-    public static void main(String[] args){
-        Main myObject = new Main();
+
+    public Main(int y) {
+        x = y;
+    }
+
+    public Main(int y, int u) {
+        x = y * u;
+    }
+
+    public static void main(String[] args) {
+        Main myObject = new Main(45);
+        Main secoundObject = new Main(10,5);
         System.out.println(myObject.x);
+        System.out.println(secoundObject.x);
     }
 }
