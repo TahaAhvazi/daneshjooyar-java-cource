72744bb67abbc540fbce4d824a85307d71e1a884
Taha Ahvazi <tahax.dev@gmail.com>
2023-11-30 00:40:48 +0330

    Java User Input (Scanner)

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,22 @@
 import java.util.Scanner;
 
-//---------------💪Java Enums💪-----------------------
-// Enums are some special classes -> Final
-// How to create an Enum ? -> enum
-enum Level {
-    LOW,
-    MEDIUM,
-    HIGH
-}
-
+//------------💪Java User Input (Scanner)💪-----------------------
 public class Main {
     public static void main(String[] args) {
-//        Level myValue = Level.LOW;
-//        System.out.println(myValue);
-
-        while (true) {
-            System.out.println("-----------");
-            System.out.println("Enter your level:");
-            System.out.print("-----------\n");
-            Scanner myScanner = new Scanner(System.in);
-            Level value = Level.HIGH;
-            String userInput = myScanner.nextLine();
-
-            if (userInput.equals("low") || userInput.equals("Low")) {
-                value = Level.LOW;
-            } else if (userInput.equals("medium") || userInput.equals("Medium")) {
-                value = Level.MEDIUM;
-            } else if(userInput.equals("high") || userInput.equals("High")){
-               value = Level.HIGH;
-            } else if (userInput.equals("exsit") || userInput.equals("Exsit")) {
-                break;
-            }else {
-                break;
-            }
-            switch (value) {
-                case LOW:
-                    System.out.println("Low level");
-                    break;
-                case MEDIUM:
-                    System.out.println("Medium level");
-                    break;
-                case HIGH:
-                    System.out.println("High level");
-                    break;
-            }
-        }
+        Scanner scannerObject = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String username = scannerObject.nextLine();
+        System.out.println("Enter your age:");
+        int userAge = scannerObject.nextInt();
+        System.out.println("Enter your height:");
+        double userHeight = scannerObject.nextDouble();
+        System.out.println("Enter your height:");
+        double userHeight1 = scannerObject.nextByte();
+        System.out.println(username);
+        System.out.println(userAge);
     }
 }
+
 
 
 
