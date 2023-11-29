5a9ebbda9131cf7763e499180879eed92cc43d34
Taha Ahvazi <tahax.dev@gmail.com>
2023-11-29 22:31:44 +0330

    Java Interface

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,34 @@
-//---------------💪Java Abstraction💪-----------------------
-// This is a super-class include all animals
-abstract class Animal {
-    public abstract void animalSound();
-    public void sleep(){
-        System.out.println(
-                "Zzz"
-        );
-    }
+//---------------💪Java Interface💪-----------------------
+// abstraction -> interface
+// Let's create our interface
+interface Thing {
+    public void callMe();
 }
-class Pig extends Animal {
+interface Animal {
+    public void animalSound();
+    public void run();
+}
+// Let's use the animal interface for our Pig class
+class Pig implements Thing , Animal {
+    @Override
+    public void callMe() {
+        System.out.println("Called");
+    }
     @Override
     public void animalSound(){
         System.out.println("wee wee");
     }
+    @Override
+    public void run(){
+        System.out.println("the animal is running");
+    }
 }
 public class Main {
     public static void main(String[] args){
-        Pig myPigObject = new Pig();
-        myPigObject.animalSound();
+        Pig pigObject = new Pig();
+        pigObject.animalSound();
+        pigObject.run();
+        pigObject.callMe();
     }
 }
 
