ca0927e25dd2171867ea3ab4fe8685729e0b64a0
Taha Ahvazi <tahax.dev@gmail.com>
2023-11-30 14:35:19 +0330

    Java ArrayList - part3

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,18 @@
-//------------💪 Java ArrayList - part2 💪-----------------------
-// Java Built-in -> Array -> Fixed size
-// Java package -> Array List -> Resizeable array -> Java package
+//------------💪 Java ArrayList - part3  💪-----------------------
+// Built-in  package - > Java - > Collections
+import java.util.Collections;
 import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> cars = new ArrayList<Integer>();
-        cars.add(23); //0
-        cars.add(110); //1
-        cars.add(56); //2
-        cars.add(76); // 3
-        cars.set(2,44);  //2
-        cars.add(89); // 4
-        cars.remove(4);
-        //cars.clear();
-//        System.out.println(cars.size());
-        System.out.println(cars);
-//        for (int i = 0; i< cars.size(); i++){
-//            System.out.println(cars.get(i));
-//        }
-        for (int i : cars){
-            System.out.println(i);
-        }
+        ArrayList<Integer> myShuffledNumbers = new ArrayList<Integer>();
+        myShuffledNumbers.add(33);
+        myShuffledNumbers.add(100);
+        myShuffledNumbers.add(20);
+        myShuffledNumbers.add(99);
+        myShuffledNumbers.add(5);
+        System.out.println("Unsorted array is: "+myShuffledNumbers);
+        Collections.sort(myShuffledNumbers);
+        System.out.println("Sorted array is: " + myShuffledNumbers );
     }
 }
 
