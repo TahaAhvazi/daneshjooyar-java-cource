7fa36551fbcbc71dc2457fabd977e35003dc69eb
Taha Ahvazi <tahax.dev@gmail.com>
2023-11-30 14:22:49 +0330

    Java ArrayList - part2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//------------💪 Java ArrayList - part1 💪-----------------------
+//------------💪 Java ArrayList - part2 💪-----------------------
 // Java Built-in -> Array -> Fixed size
 // Java package -> Array List -> Resizeable array -> Java package
 import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> cars = new ArrayList<String>();
-        cars.add("Volvo"); //0
-        cars.add("BMW"); //1
-        cars.add("Saipa"); //2
-        cars.add("Mazda"); // 3
-        System.out.println(cars.get(2));
+        ArrayList<Integer> cars = new ArrayList<Integer>();
+        cars.add(23); //0
+        cars.add(110); //1
+        cars.add(56); //2
+        cars.add(76); // 3
+        cars.set(2,44);  //2
+        cars.add(89); // 4
+        cars.remove(4);
+        //cars.clear();
+//        System.out.println(cars.size());
+        System.out.println(cars);
+//        for (int i = 0; i< cars.size(); i++){
+//            System.out.println(cars.get(i));
+//        }
+        for (int i : cars){
+            System.out.println(i);
+        }
     }
 }
 
