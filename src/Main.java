134512c2af81fe3c0ae64e2577aed9ff9cffcfb6
Taha Ahvazi <tahax.dev@gmail.com>
2023-12-01 17:47:20 +0330

    Java Exceptions {TryCatchFinally}

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,32 @@
-//------------💪 Java Wrapper Classes 💪-----------------------
-// ArrayList<Integer>
+//------💪Java Exceptions {Try🟢Catch🟢Finally}💪-----------------------
+// try {
+//      // mmy code
+        // my code
+// }catch(Exception e){
+//      Show a  custom error -> e
+// }finally{
+//      // do whatever you wanna do ->(Optional)
+// }
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int myNumber = 5;
-        Integer myNumber1 = 6;
-        System.out.println(myNumber);
-        String myString = myNumber1.toString();
-        System.out.println(myString);
+//        try{
+//            int[] myNumber = {0,1,2,3,4,5,6,7,8,9};
+//            System.out.println(myNumber[7]);
+//        }catch (Exception e){
+//            System.out.println("\uD83D\uDFE2 Error out of range \uD83D\uDFE2");
+//        }finally {
+//            System.out.println("The application job is done!");
+//        }
+        while (true){
+            Scanner myScanner = new Scanner(System.in);
+            System.out.println("Enter your age: \n");
+            int userAge = myScanner.nextInt();
+            CheckAge myObject = new CheckAge();
+            myObject.checkAge(userAge);
+        }
     }
 }
 
