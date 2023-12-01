b53e644d7247bf3580f2d2284615e40ea75e5214
Taha Ahvazi <tahax.dev@gmail.com>
2023-12-01 22:29:58 +0330

    Pproblem solving part-3

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,28 @@
-//-------------💡 Solving problems 💡 -----------------------
+//-------------💡 Solving problems - part3 💡 -----------------------
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int number1,number2,result;
         Scanner myScannerObject = new Scanner(System.in);
-        System.out.println("Enter the first number:");
-        number1 = myScannerObject.nextInt();
-        System.out.println("Enter the secound number: ");
-        number2 = myScannerObject.nextInt();
-
-        result = number2 * number1;
-        System.out.println("------------------");
-        System.out.println("---result:" + result + "-----");
-        System.out.println("------------------");
+        // Ask the user for number of  elements
+        System.out.println("Enter the number of elements:");
+        int ArrayLength = myScannerObject.nextInt();
+        // Let's create our array ...
+        int[] myArray = new int[ArrayLength];
+        // Receive the numbers from the user
+        System.out.println("-----Enter the numbers-----");
+        for (int i = 0; i<ArrayLength; i++){
+            System.out.println("Element no " + (i+1) + ": ");
+            myArray[i] = myScannerObject.nextInt();
+        }
+        // Let's calculate the sum of elements from user
+        int sum = 0;
+        for (int i = 0; i<myArray.length; i++){
+            sum = sum + myArray[i];
+        }
+        //Print the result of sum
+        System.out.println(sum);
+        // Close the scanner to prevent resources leak
+        myScannerObject.close();
     }
 }
 
