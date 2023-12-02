62bf12e93cccdda407ca9604894ea56d7ab2ecc6
Taha Ahvazi <tahax.dev@gmail.com>
2023-12-02 12:19:24 +0330

    Solving problems- Generate prime numbers

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-//-------------💡Solving problems - Palindrome💡 -----------------------
+//-------------💡Solving problems  - Prime number generator💡-----------------------
 public class Main {
     public static void main(String[] args) {
     }
