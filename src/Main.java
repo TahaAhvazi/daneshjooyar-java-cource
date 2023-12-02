2e3edf30de6211b58d6b0ee258fbd1220239a2ad
Taha Ahvazi <tahax.dev@gmail.com>
2023-12-02 11:56:18 +0330

    Solving problems - Palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,6 @@
-//-------------💡Java Lambda Expressions💡 -----------------------
-// Java version 8
-// Lambda -> short of a code block - > parameters -> return value
-// parameters -> expression
-// (parameter1 , parameter2) -> expression
-// (parameter1 , parameter2) -> {code bloc}
-import java.util.ArrayList;
-import java.util.function.Consumer;
-interface StringFunction {
-    String run(String str);
-}
+//-------------💡Solving problems - Palindrome💡 -----------------------
 public class Main {
-    public static void printFormatted(String str, StringFunction format){
-        String result  = format.run(str);
-        System.out.println(result);
-    }
     public static void main(String[] args) {
-        StringFunction exclaim = (s) -> s + "!";
-        StringFunction ask = (s) -> s + "?";
-        printFormatted("Your shirt is blue", exclaim);
-        printFormatted("Your shirt is red", ask);
     }
 }
 
